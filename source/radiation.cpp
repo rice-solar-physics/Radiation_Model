b354a2a32d13fd77c67381a4f69cd245ea4a9111
Will Barnes <wtb2@rice.edu>
2016-03-02 22:27:42 -0600

    bug fix

--- source/radiation.cpp
+++ source/radiation.cpp
@@ -87,7 +87,7 @@ void CRadiation::Initialise(char *szFilename, bool doEmissCalc)
 	    sprintf( szRatesFilename,"%s%s.rts",tempRates,tempSymb);
 	    sprintf( szIonFracFilename,"%s%s.bal",tempBalances,tempSymb);
 	    // Instantiate each element object
-	    ppElements[i] = new CElement( pZ[i], szRangesFilename, szAbundFilename, szEmissFilename, szRatesFilename, szIonFracFilename doEmissCalc );
+	    ppElements[i] = new CElement( pZ[i], szRangesFilename, szAbundFilename, szEmissFilename, szRatesFilename, szIonFracFilename, doEmissCalc );
 		// Set initial configuration elements from file
 		ppElements[i]->SetConfigVars(root);
 		//Increment counter
