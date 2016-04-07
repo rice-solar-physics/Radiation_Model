47d1721ba7080007ac6f0be61324ba0aae52bba1
Will Barnes <wtb2@rice.edu>
2016-04-07 18:08:15 -0500

    trying without static build

--- build_docs.py
+++ build_docs.py
@@ -16,7 +16,7 @@ sources=glob.glob('source/*.cpp') + glob.glob('source/OpticallyThick/*.cpp')
 headers=glob.glob('source/*.h') + glob.glob('source/OpticallyThick/*.h')
 
 #check for travis
-on_travis = os.environ.get('TRAVIS',None)=='True'
+on_travis = os.environ.get('TRAVIS',None).lower()=='true'
 
 #Set build options
 if on_travis:
