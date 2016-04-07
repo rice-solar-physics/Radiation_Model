26d48b3e16532ba242dfbe83c30bfdd47dae107d
Will Barnes <wtb2@rice.edu>
2016-04-07 18:04:24 -0500

    trying without static build

--- build_docs.py
+++ build_docs.py
@@ -8,14 +8,21 @@
 #TODO: docs page on github
 
 import glob
+import os
 import subprocess
 
 #Get all headers and sources
 sources=glob.glob('source/*.cpp') + glob.glob('source/OpticallyThick/*.cpp')
 headers=glob.glob('source/*.h') + glob.glob('source/OpticallyThick/*.h')
 
+#check for travis
+on_travis = os.environ.get('TRAVIS',None)=='True'
+
 #Set build options
-build_opts = '--static --report --merge docs/ext --output docs/html '
+if on_travis:
+    build_opts = '--report --merge docs/ext --output docs/html '
+else:
+    build_opts = '--static --report --merge docs/ext --output docs/html '
 build_opts += ' '.join(sources) + ' ' + ' '.join(headers)
 
 #Set needed CXX flags
